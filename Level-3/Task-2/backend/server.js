cfba1a2b63a01363ee85a97d4c2a488d371ee37e
Ishitasurat25 <ishitasurati83@gmail.com>
2025-03-02 15:39:48 +0530

    deploy

--- Level-3/Task-2/backend/server.js
+++ Level-3/Task-2/backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/",(req,res)=>{
+    console.log("Welcome to backend");
+    
+})
 app.use("/api/internships", internshipRoutes);
 
 app.listen(3000, () => console.log("🚀 Server running on port 3000"));
