8590fb791998514c8c79ab343f7d421af4a129e2
Ishitasurat25 <ishitasurati83@gmail.com>
2025-03-02 15:47:12 +0530

    deploy

--- Level-3/Task-2/backend/server.js
+++ Level-3/Task-2/backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/",(req,res)=>{
-    console.log("Welcome to backend");
+   send.res("Welcome to backend");
     
 })
 app.use("/api/internships", internshipRoutes);
